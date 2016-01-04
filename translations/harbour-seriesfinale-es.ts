62957fd2b6adc36974180ee33141fabd9f1f7d33
Carmen F. B <carmenfdezb@yahoo.es>
2016-01-04 20:58:59 +0100

    Add translate to Spanish

---
+++ translations/harbour-seriesfinale-es.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>AddShow</name>
+    <message>
+        <source>Add show</source>
+        <translation>Añadir programa</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <source>Show sorting</source>
+        <translation>Orden de los programas</translation>
+    </message>
+    <message>
+        <source>Add special seasons</source>
+        <translation>Añadir temporadas especiales</translation>
+    </message>
+    <message>
+        <source>Episode sorting</source>
+        <translation>Orden de los episodios</translation>
+    </message>
+    <message>
+        <source>Seasons sorting</source>
+        <translation>Orden de las temporadas</translation>
+    </message>
+    <message>
+        <source>Sort by genre</source>
+        <translation>Ordenar por género</translation>
+    </message>
+</context>
+</TS>
