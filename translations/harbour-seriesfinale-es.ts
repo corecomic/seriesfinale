588afa2349bd920c9ecfda7d963aa8409b815aa5
Carmen F. B <carmenfdezb@yahoo.es>
2016-01-06 20:19:13 +0100

    Added new translation to Spanish

--- translations/harbour-seriesfinale-es.ts
+++ translations/harbour-seriesfinale-es.ts
@@ -4,8 +4,104 @@
 <context>
     <name>AddShow</name>
     <message>
+        <source>Search options</source>
+        <translation>Opciones de búsqueda</translation>
+    </message>
+    <message>
         <source>Add show</source>
         <translation>Añadir programa</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+</context>
+<context>
+    <name>EpisodePage</name>
+    <message>
+        <source>Air date:</source>
+        <translation>Fecha de emisión:</translation>
+    </message>
+    <message>
+        <source>Rating:</source>
+        <translation>Calificación:</translation>
+    </message>
+    <message>
+        <source>Overview:</source>
+        <translation>Resumen:</translation>
+    </message>
+    <message>
+        <source>Watched</source>
+        <translation>Visto</translation>
+    </message>
+</context>
+<context>
+    <name>SearchSettingsPage</name>
+    <message>
+        <source>Search options</source>
+        <translation>Opciones de búsqueda</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Idioma</translation>
+    </message>
+</context>
+<context>
+    <name>SeasonPage</name>
+    <message>
+        <source>Mark all</source>
+        <translation>Marcar todo</translation>
+    </message>
+    <message>
+        <source>Mark none</source>
+        <translation>Desmarcar todo</translation>
+    </message>
+    <message>
+        <source>No episodes</source>
+        <translation>No hay episodios</translation>
+    </message>
+</context>
+<context>
+    <name>SeriesPage</name>
+    <message>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Refreshing...</source>
+        <translation>Actualizando...</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <source>Add Show</source>
+        <translation>Añadir programa</translation>
+    </message>
+    <message>
+        <source>Mark next episode</source>
+        <translation>Marcar siguiente episodio</translation>
+    </message>
+    <message>
+        <source>Mark show as watched</source>
+        <translation>Marcar programa como visto</translation>
+    </message>
+    <message>
+        <source>Delete show</source>
+        <translation>Borrar programa</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Borrando</translation>
+    </message>
+    <message>
+        <source>No shows</source>
+        <translation>No hay programas</translation>
     </message>
 </context>
 <context>
@@ -19,24 +115,75 @@
         <translation>Guardar</translation>
     </message>
     <message>
+        <source>Sorting</source>
+        <translation>Orden</translation>
+    </message>
+    <message>
         <source>Show sorting</source>
         <translation>Orden de los programas</translation>
     </message>
     <message>
-        <source>Add special seasons</source>
-        <translation>Añadir temporadas especiales</translation>
+        <source>By title</source>
+        <translation>Por título</translation>
     </message>
     <message>
-        <source>Episode sorting</source>
-        <translation>Orden de los episodios</translation>
+        <source>By next episode date</source>
+        <translation>Por fecha del siguiente episodio</translation>
+    </message>
+    <message>
+        <source>By last aired episode</source>
+        <translation>Por último episodio emitido</translation>
     </message>
     <message>
         <source>Seasons sorting</source>
         <translation>Orden de las temporadas</translation>
     </message>
     <message>
+        <source>Episode sorting</source>
+        <translation>Orden de los episodios</translation>
+    </message>
+    <message>
         <source>Sort by genre</source>
         <translation>Ordenar por género</translation>
+    </message>
+    <message>
+        <source>Add special seasons</source>
+        <translation>Añadir temporadas especiales</translation>
+    </message>
+</context>
+<context>
+    <name>ShowPage</name>
+    <message>
+        <source>Refreshing...</source>
+        <translation>Actualizando...</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Información</translation>
+    </message>
+    <message>
+        <source>Mark None</source>
+        <translation>Desmarcar todo</translation>
+    </message>
+    <message>
+        <source>Mark All</source>
+        <translation>Marcar todo</translation>
+    </message>
+    <message>
+        <source>Delete season</source>
+        <translation>Borrar temporada</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Borrando</translation>
+    </message>
+    <message>
+        <source>No seasons</source>
+        <translation>No hay temporadas</translation>
     </message>
 </context>
 </TS>
