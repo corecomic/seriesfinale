0c6386968563227fb1db6ca25da42a765c1eee34
Åke Engelbrektson <eson57@users.noreply.github.com>
2016-05-09 05:15:37 +0200

    Added files via upload

---
+++ translations/python-messages-sv.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message numerus="yes">
+        <location filename="src/SeriesFinale/series.py" line="268"/>
+        <source>%s season</source>
+        <translation>
+            <numerusform>%s säsong</numerusform>
+            <numerusform>%s säsonger</numerusform>
+        </translation>
+        <extra-po-msgid_plural>%s seasons</extra-po-msgid_plural>
+        <extra-po-flags>python-format</extra-po-flags>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="271"/>
+        <location filename="src/SeriesFinale/series.py" line="309"/>
+        <source>Completely watched</source>
+        <translation>Färdigsedd</translation>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="273"/>
+        <source>Show has ended</source>
+        <translation>Serien är avslutad</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="src/SeriesFinale/series.py" line="277"/>
+        <location filename="src/SeriesFinale/series.py" line="313"/>
+        <source>%s episode not watched</source>
+        <translation>
+            <numerusform>%s episod ej sedd</numerusform>
+            <numerusform>%s episoder ej sedda</numerusform>
+        </translation>
+        <extra-po-msgid_plural>%s episodes not watched</extra-po-msgid_plural>
+        <extra-po-flags>python-format</extra-po-flags>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="282"/>
+        <location filename="src/SeriesFinale/series.py" line="318"/>
+        <source>No episodes to watch</source>
+        <translation>Inga episoder att se</translation>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="286"/>
+        <source>&lt;i&gt;Next:&lt;/i&gt; %s, %s</source>
+        <translation>&lt;i&gt;Nästa:&lt;/i&gt; %s, %s</translation>
+        <extra-po-flags>python-format</extra-po-flags>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="290"/>
+        <source>&lt;i&gt;Next:&lt;/i&gt; %s</source>
+        <translation>&lt;i&gt;Nästa:&lt;/i&gt; %s</translation>
+        <extra-po-flags>python-format</extra-po-flags>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="298"/>
+        <source>Special</source>
+        <translation>Special</translation>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="300"/>
+        <source>Season %s</source>
+        <translation>Säsong %s</translation>
+        <extra-po-flags>python-format</extra-po-flags>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="322"/>
+        <source>&lt;i&gt;Next episode:&lt;/i&gt; %s, %s</source>
+        <translation>&lt;i&gt;Nästa episod:&lt;/i&gt; %s, %s</translation>
+        <extra-po-flags>python-format</extra-po-flags>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="326"/>
+        <source>&lt;i&gt;Next episode:&lt;/i&gt; %s</source>
+        <translation>&lt;i&gt;Nästa episod:&lt;/i&gt; %s</translation>
+        <extra-po-flags>python-format</extra-po-flags>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="420"/>
+        <source>Ep. %s: %s</source>
+        <translation>Ep. %s: %s</translation>
+        <extra-po-flags>python-format</extra-po-flags>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="467"/>
+        <source>Today</source>
+        <translation>I dag</translation>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="469"/>
+        <source>Tomorrow</source>
+        <translation>I morgon</translation>
+    </message>
+    <message>
+        <location filename="src/SeriesFinale/series.py" line="471"/>
+        <source>Yesterday</source>
+        <translation>I går</translation>
+    </message>
+</context>
+</TS>
